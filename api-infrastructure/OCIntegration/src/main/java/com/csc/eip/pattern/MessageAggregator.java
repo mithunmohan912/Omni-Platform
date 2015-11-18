2cae66dc24a3e28dd49ca3d86ea54a18eb267f5c
unknown <ssethia@csc.com>
2015-11-18 18:17:32 +0530

    OC-67 #time 35hr #resolved Aggregation Pattern Implementation

---
+++ api-infrastructure/OCIntegration/src/main/java/com/csc/eip/pattern/MessageAggregator.java
@@ -0,0 +1,24 @@
+package com.csc.eip.pattern;
+
+import java.net.URISyntaxException;
+import java.util.Map;
+
+import org.apache.camel.util.URISupport;
+import org.apache.log4j.Logger;
+
+public class MessageAggregator {
+	static Logger log = Logger.getLogger(MessageAggregator.class.getName());
+
+	public String aggregatePattern(String camelHttpQuery) throws URISyntaxException {
+
+		Map<String, Object> params = URISupport.parseQuery(camelHttpQuery);
+		String sor1 = (String) params.get("uri1");
+		String sor2 = (String) params.get("uri2");
+
+		log.info("messageTranslator::aggregatePattern::uri1=" + sor1);
+		log.info("messageTranslator::aggregatePattern::uri2=" + sor2);
+
+		return "\"" + sor1 + "\",\"" + sor2 + "\"";
+	}
+
+}
--- api-infrastructure/OCIntegration/src/main/java/com/csc/eip/route/EipRoutes.java
+++ api-infrastructure/OCIntegration/src/main/java/com/csc/eip/route/EipRoutes.java
@@ -4,38 +4,44 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 
 import com.csc.eip.bo.Message;
+import com.csc.eip.strategy.CollectionAggregationStrategy;
 
 /**
  * REST services defined with Camel Java REST DSL Router
  */
-public class EipRoutes extends RouteBuilder {	
+public class EipRoutes extends RouteBuilder {
 
-    @Override
-    public void configure() throws Exception {
+	@Override
+	public void configure() throws Exception {
+
+		restConfiguration().component("servlet").bindingMode(RestBindingMode.json).dataFormatProperty("prettyPrint",
+				"true");
 		
-        restConfiguration()
-			.component("servlet")
-			.bindingMode(RestBindingMode.json)
-            .dataFormatProperty("prettyPrint", "true");
-		
-        rest("/OCIntegration")
-			.description("Pattern replace service")
-            .consumes("application/json")
+		  
+		rest("/OCIntegration")
+			.description("URI Transformation Service")
+			.consumes("application/json")
 			.produces("application/json")
-		
-			.post()
-				.description("Replaces all pattern matches in a JSON message")
-				.type(Message.class)
-				.outType(Message.class)
-		    	// URL params are not automatically added to Header params for POSTs
-		    	// use CamelHttpQuery header to get URL params
-				.to("bean:OCIntegration?method=replacePattern(${body})");
-        rest("/OCIntegration")
-        .description("Pattern replace service 2")
-		.get()
-			.description("Get the content")
+
+		.post()
+			.description("Replaces all pattern matches in a JSON message")
+			.type(Message.class)
 			.outType(Message.class)
-			.to("bean:OCIntegration?method=writeMessage(${in.headers[CamelHttpQuery]})");
+			.to("bean:OCIntegration?method=replacePattern(${body})");
+
+		rest("/OCIntegration")
+			.description("Aggregation Service")
+			.produces("application/json")
+		.get()
+			.outType(Message.class)
+			.route()
+				.removeHeaders("CamelHttp*")
+				.enrich("direct:serviceAggregate");
+	
+		from("direct:serviceAggregate")
+			.recipientList(header("URIS").tokenize(","))
+			.aggregationStrategy(new CollectionAggregationStrategy()).parallelAggregate()
+		.end();
 
     }
 
