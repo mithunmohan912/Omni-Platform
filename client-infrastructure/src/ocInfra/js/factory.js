3a6707a1aaf132c37942c74d3c62d9a8460f6a7c
unknown <pbachu3@CSCINDAG979597.asiapac.globalcsc.net>
2016-04-05 16:14:42 +0530

    small changes error

--- client-infrastructure/src/ocInfra/js/factory.js
+++ client-infrastructure/src/ocInfra/js/factory.js
@@ -253,7 +253,7 @@ function httpMethodToBackEnd($scope, dataFactory, $rootScope, options, resolve){
                 listDispScope.stTableList = [];
                 listDispScope.showResult = false;
             }
-        }).error(function(data){
+        }).error(function(){
             $rootScope.loader.loading=false;
         });
     } else if(httpmethod==='POST'){
@@ -269,7 +269,7 @@ function httpMethodToBackEnd($scope, dataFactory, $rootScope, options, resolve){
                     resolve();
                 }
             }
-        }).error(function(data){
+        }).error(function(){
             $rootScope.loader.loading=false;
         });
     } else if(httpmethod==='PATCH'){
@@ -282,7 +282,7 @@ function httpMethodToBackEnd($scope, dataFactory, $rootScope, options, resolve){
                     resolve();
                 }
             }
-        }).error(function(data){
+        }).error(function(){
             $rootScope.loader.loading=false;
         });
     }
