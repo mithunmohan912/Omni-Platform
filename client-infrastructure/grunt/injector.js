feb0a66c888d564b036d9fa380a9c56c4e936439
Anil Kumar Gurram <agurram2@asiapac.globalcsc.net>
2016-04-28 15:38:25 +0530

    OC-359, OC-362,  OC-364, fixed

--- client-infrastructure/grunt/injector.js
+++ client-infrastructure/grunt/injector.js
@@ -31,7 +31,6 @@ module.exports = {
                                              'src/app-miniquote/ocInfra/js/ocInfra.js',
                                              'src/ocInfra/css/*.css',
                                              'src/ocInfra/css/Generic_Iconfont/font/generic_flaticon.css',
-                                             'src/app-miniquote/ocInfra/css/ocInfra.min.css',
                                              'src/ocInfra/css/Omni_Iconfont/font/flaticon.css'
             ],
             'src/app-weather/index.html': ['src/app-weather/js/*.js',
