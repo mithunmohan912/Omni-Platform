373f7a87a0c2e5a9536dfe06336c05369e4dbeba
Adolfo Fernandez Suarez <afernandezsu@csc.com>
2016-05-06 16:45:03 +0200

    Including resource directory into resource factory

--- client-infrastructure/src/ocInfra/js/resource.js
+++ client-infrastructure/src/ocInfra/js/resource.js
@@ -3,74 +3,160 @@
 global app
 */
 
+app.factory('resourceFactory', ['$http', '$rootScope', '$q', function($http, $rootScope, $q) {
 
-    app.factory('resourceFactory', ['$http', function($http) {
+    var resourceDirectory = {};
 
-    var resourceFactory = {};
+    function _get(url, params, headers) {
+        var promise;
+        if (!resourceDirectory[url]) {
+            promise = $http({
+                    method : 'GET',
+                    url : url,
+                    data : params,
+                    headers : headers
+            });
+            if (promise.then) {
+                promise.then(function(response) {
+                    resourceDirectory[url] = response.data;
+                    $rootScope.$broadcast('resourceDirectory', { 'url': url, 'response': response });
 
-    resourceFactory.getData = function (urlBase) {
-        return $http.get(urlBase);
-    };
-
-    resourceFactory.get = function (urlBase,params,headers) {
-        var obj =   $http(
-            {
-                method : 'GET',
-                url : urlBase,
-                params : params,
-                headers : headers
+                }, function(error) {
+                    console.error(error);
+                    throw error;
+                });   
             }
-        );   
-        return obj;
-    };
+        } else {
+            promise = $q(function(resolve) {
+                resolve(resourceDirectory[url]);
+            });
+            promise.success = function(callback) {
+                var _success = callback;
+                promise.then(_success, null);
+                return promise;
+            };
+            promise.error = function(callback) {
+                var _error = callback;
+                promise.then(null, _error);
+                return promise;
+            };
+        }
+        return promise;
+    }
 
-    resourceFactory.post = function (urlBase,params,headers) {
-        var obj = $http({
+    function _refresh(url, params, headers) {
+        resourceDirectory[url] = null;
+        return _get(url, params, headers);
+    }
+
+    function _post(url, params, headers) {
+        var promise = $http({
                 method: 'POST',
-                url: urlBase,
+                url: url,
                 headers: headers,
                 data: params
-            });
-        return obj;
-    };
+        });
+        if (promise.then) {
+            promise.then(function(response) {
+                resourceDirectory[url] = response.data;
+                $rootScope.$broadcast('resourceDirectory', { 'url': url, 'response': response });
 
-    resourceFactory.insert = function (urlBase,obj) {
-        return $http.post(urlBase, obj);
-    };
+            }, function(error) {
+                console.error(error);
+                throw error;
+            });   
+        }
+        return promise;
+    }
 
-    resourceFactory.update = function (urlBase,obj) {
-        return $http.put(urlBase + '/' + obj.id, obj);
-    };
-
-    resourceFactory.delete = function (url, headers) {
-        var obj = $http({
+    function _delete(url, headers) {
+        var promise = $http({
             method : 'DELETE',
-            url : url,
+            ul : url,
             headers : headers
         });
-        return obj;
-    };
+        if (promise.then) {
+            promise.then(function(response) {
+                resourceDirectory[url] = null;
+                $rootScope.$broadcast('resourceDirectory', { 'url': url, 'response': response });
 
-    resourceFactory.options=function(urlBase, headers){
-        var obj =   $http(
-            {
-                method : 'GET',
-                url : urlBase,
-                headers : headers
-            }
-        );   
-        return obj;
-    };
+            }, function(error) {
+                console.error(error);
+                throw error;
+            });   
+        }
+        return promise;
+    }
 
-    resourceFactory.patch = function (urlBase,params,headers) {
-        var obj = $http({
+    function _patch(url,params,headers) {
+        var promise = $http({
                 method: 'PATCH',
-                url: urlBase,
+                url: url,
                 headers: headers,
                 data: params
-            });
-        return obj;
+        });
+        if (promise.then) {
+            promise.then(function(response) {
+                resourceDirectory[url] = response.data;
+                $rootScope.$broadcast('resourceDirectory', { 'url': url, 'response': response });
+
+            }, function(error) {
+                console.error(error);
+                throw error;
+            }); 
+        } 
+        return promise;
+    }
+
+    function _execute(url, params, headers, method) {
+        var promise = $http({
+                method: method,
+                url: url,
+                headers: headers,
+                data: params
+        });
+        if (promise.then) {
+            promise.then(function(response) {
+                resourceDirectory[url] = response;
+                $rootScope.$broadcast('resourceDirectory', { 'url': url, 'response': response });
+
+            }, function(error) {
+                console.error(error);
+                throw error;
+            }); 
+        } 
+        return promise;
+
+    }
+
+    return {
+        'get': _get,
+        'refresh': _refresh,
+        'post': _post,
+        'delete' : _delete,
+        'patch': _patch,
+        'execute': _execute,
+
+        'getData' : function (urlBase) {
+            return $http.get(urlBase);
+        },
+        'insert' : function (urlBase,obj) {
+            return $http.post(urlBase, obj);
+        },
+
+        'update' : function (urlBase,obj) {
+            return $http.put(urlBase + '/' + obj.id, obj);
+        },
+        'options' : function(urlBase, headers){
+            var obj =   $http(
+                {
+                    method : 'GET',
+                    url : urlBase,
+                    headers : headers
+                }
+            );   
+            return obj;
+        }
     };
- return resourceFactory;
 
 }]);
