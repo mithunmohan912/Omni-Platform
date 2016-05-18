f6915cb2f53dc10669cb40aaa64e5ecd39c7e870
Sourabh Ranka <sranka2@asiapac.globalcsc.net>
2016-05-18 21:46:49 +0530

    updates in galen scripts resolving multiple id issues on same page

--- client-infrastructure/tests/galen1/Pages/FirstPage.js
+++ client-infrastructure/tests/galen1/Pages/FirstPage.js
@@ -1,5 +1,5 @@
 this.LandingPage = $page("Landing page", {
-  loginButton: "id: login",
+  loginButton: "xpath: /html/body/div[5]/div/div/ng-include/form/div/div[2]/div[2]/div/div[4]/div/ng-switch/div/div/div/div/div[2]/div/div[2]/div/ng-switch[2]/div/div[5]/ng-switch[2]/div/button",
  
   clickLoginButton: function () {
     this.loginButton.click();
--- client-infrastructure/tests/galen1/Pages/LoginPage.js
+++ client-infrastructure/tests/galen1/Pages/LoginPage.js
@@ -1,6 +1,6 @@
 this.LoginPage = $page("Login page", {
   userid: "id: inputUsername", // css locator
-  password: "id: inputPassword",
+  password: "xpath: /html/body/div[5]/div/div/ng-include/form/div/div[2]/div[2]/div/div[4]/div/ng-switch/div/div/div/div/div[2]/div/div[2]/div/ng-switch[2]/div/div[2]/ng-switch[2]/div/input",
   signinButton: "id: submit",
                         
   
