b10d6845025f9546f03ed39432ea2e32b8cf5099
Adolfo Fernandez Suarez <afernandezsu@csc.com>
2016-05-23 18:00:18 +0200

    App reference updates in factories and styles

--- client-infrastructure/src/app-reference/app/coverageFactory.js
+++ client-infrastructure/src/app-reference/app/coverageFactory.js
@@ -6,6 +6,20 @@ global app
 
 app.factory('coverageFactory', function($rootScope, $location){
 	return {
+		next: function(resource) {
+			resourceFactory.refresh(resource.href + '/operations/transferToOffer/status_report').then(function(response) {
+				var data = response.data || response;
+				if (data.consistent) {
+					resourceFactory.post(resource.href + '/operations/transferToOffer/execute', {}, $rootScope.headers).then(function(response) {
+						$rootScope.resourceUrl = response.headers()['content-location'];
+						$location.path('/screen/offer');
+					});
+				} else {
+					$rootScope.errordata = data.messages;
+					$rootScope.showError = true;
+				}
+			});
+		},
 		back: function() {
 			$location.path('/screen/quote');
 		}
