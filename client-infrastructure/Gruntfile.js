65497b988f051aaede3d1e4252d69f3b52dacdbc
Rahul Kalidindi <rkalidindi2@csc.com>
2016-05-27 17:19:00 +0530

    OC - 462. Automating the Cordova Project creation using the shell script. Also updated the Readme file with the steps involved

--- client-infrastructure/Gruntfile.js
+++ client-infrastructure/Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
 	
 	grunt.registerTask('build4m', ['copy4m']);
 	
-	grunt.registerTask('buildMobilePkg', ['clean:mobile','copy:dist_mobile','replace:replace_mobile','replace:replace_mobile_appjs']);
+	grunt.registerTask('buildMobilePkg', ['clean:mobile','copy:dist_mobile','replace:replace_mobile','replace:replace_mobile_appjs','clean:cordova','copy:dist_cordova','clean:mobile']);
 
 	grunt.registerTask('exportTemplates', [
   		'ngtemplates'
