a95c16d3cc4cc417e3413e2e2306952eeb20cc48
Adolfo Fernandez Suarez <afernandezsu@csc.com>
2016-06-01 18:48:21 +0200

    Updates to app-reference

---
+++ client-infrastructure/src/app-reference/app/optionsController.js
@@ -0,0 +1,33 @@
+/*
+global app
+*/
+
+app.controller('OptionsController', [ '$scope', function($scope){
+
+	function init(){
+		$scope.optionsTable = [];
+
+		var allOptions = [];
+
+		for(var item in $scope.resourcesToBind[column.id].items){
+			allOptions = allOptions.concat($scope.resultSet[item.href].properties['options_table'].value);
+		}
+
+		$scope.optionsTable = allOptions;
+	}
+
+	console.error($scope.resultSet);
+	console.error($scope.resourcesToBind);
+
+	$scope.$watch('resourcesToBind', function(newValue){
+		if(newValue) {
+			init();
+		}
+	}, true);
+
+	$scope.$watch('resultSet', function(newValue){
+		if(newValue) {
+			init();
+		}
+	}, true);
+}]);
