01f1a6dd3b1d8a1754c8f7292fafeb010bcade9b
Sourabh Ranka <sranka2@csc.com>
2016-06-03 20:59:27 +0530

    oc-426 running protractor from jenkins

---
+++ client-infrastructure/grunt/protractor_webdriver.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    options: {
+      // Task-specific options go here. 
+    },
+    startselenium: {
+      // Target-specific file lists and/or options go here. 
+      options: {
+        command: 'webdriver-manager start'
+      }
+    }
+ 
+};
