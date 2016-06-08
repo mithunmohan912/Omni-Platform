8b6134092fe1dc84c5de938080d79c4153ceec7c
Adolfo Fernandez Suarez <afernandezsu@csc.com>
2016-06-08 12:53:59 +0200

    Updating app-reference application

--- client-infrastructure/src/app-reference/app/dashboardFactory.js
+++ client-infrastructure/src/app-reference/app/dashboardFactory.js
@@ -4,11 +4,31 @@
 global app
 */
 
-app.factory('dashboardFactory', function($rootScope){
+app.factory('dashboardFactory', function($rootScope, resourceFactory){
 	return {
 		dashboardToQuote: function(resource){
 			$rootScope.resourceUrl= resource.href;
 			$rootScope.navigate('/screen/quote');
+		},
+		createQuote: function() {
+			//example car
+			var payload = {
+				'quote:distributor_id': "998877", 
+				'quote:product_id': "MC011"
+			}
+			resourceFactory.post($rootScope.hostURL+'quotes', payload, $rootScope.headers).then(function(response) {
+				$rootScope.resourceUrl= response.data._links.self.href;
+				$rootScope.navigate('/screen/quote');
+			});	
+		},
+		searchQuotes: function(resource, properties) {
+			var payload = {};
+			for (var prop in properties) {
+				if (properties[prop].value) {
+					payload[prop] = properties[prop].value;
+				}
+			}
+			resourceFactory.refresh(resource.href, payload);
 		}
 	};
 });
