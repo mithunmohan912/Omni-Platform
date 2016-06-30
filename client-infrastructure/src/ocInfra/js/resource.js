12bb1d7dec34b1b3ef649c4e11a7b40ca319d70e
unknown <ssethia@csc.com>
2016-06-30 19:37:58 +0530

    OC-552 - Metamodel changes quote create/edit screen. Also rolling back fix from Minh. Fixed the header html to be displayed on all screens excet landing and login screen.

--- client-infrastructure/src/ocInfra/js/resource.js
+++ client-infrastructure/src/ocInfra/js/resource.js
@@ -7,8 +7,6 @@ app.factory('resourceFactory', ['$http', '$rootScope', '$q', function($http, $ro
 
     var resourceDirectory = {};
     var PENDING_REQUEST = '0';
-    //used to distinct between GET method with and without params
-    var urlParams = {};
 
     function _addApiGatewayApiKeys(params) {
         if (params === undefined) {
@@ -26,10 +24,6 @@ app.factory('resourceFactory', ['$http', '$rootScope', '$q', function($http, $ro
         // Since the url params are not considered when updating the resource directory, we just reset it for the concrete URL if we have params
         if(params && Object.keys(params).length > 0){
             resourceDirectory[url] = null;
-            urlParams[url] = true;
-        }else if(urlParams[url]){
-            resourceDirectory[url] = null;
-            urlParams[url] = false;
         }
 
         params = _addApiGatewayApiKeys(params);
