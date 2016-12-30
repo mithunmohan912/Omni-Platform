fe00068165e585131b428529fb43a8ecb85c7ba0
unknown <ssethia@csc.com>
2016-12-30 17:33:51 +0530

    OC-1558 - Tabs redesigned with type as tabs2

--- client-infrastructure/src/ocInfra/js/app.js
+++ client-infrastructure/src/ocInfra/js/app.js
@@ -84,3 +84,20 @@ function showHostErrorMessage(message, severity) {
     $('#errorMessage').html(message);
     $('#errorPopup').show();
 }
+
+app.directive('scrollTo', function ($location, $anchorScroll) {
+  return function(scope, element, attrs) {
+
+    element.bind('click', function(event) {
+        event.stopPropagation();
+        var off = scope.$on('$locationChangeStart', function(ev) {
+            off();
+            ev.preventDefault();
+        });
+        var location = attrs.scrollTo;
+        $location.hash(location);
+        $anchorScroll();
+    });
+
+  };
+});
